0db92b7b8674d1ad04c0228d73224752d6157fc3
liying <hello@liying.cn>
2024-02-27 08:43:43 +0800

    Changes

---
+++ src/main/java/com/liying/back/graducation/util/QRCodeUtil.java
@@ -0,0 +1,127 @@
+package com.liying.back.graducation.util;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import javax.imageio.ImageIO;
+
+import com.google.zxing.*;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+/**
+ * 二维码相关工具方法
+ *
+ * @Author: liying
+ * @Date: 2024/2/26 5:13
+ * @Description:
+ */
+public class QRCodeUtil {
+
+    /**
+     * 生成指定网址大小及图片格式的二维码
+     *
+     * @param url      网址
+     * @param size     尺寸
+     * @param fileType 二维码图片格式
+     * @param qrFile   生成图片保存地址
+     */
+    private static void createQRCode(String url, int size, String fileType, File qrFile)
+            throws WriterException, IOException {
+
+        HashMap<EncodeHintType, Object> hintMap = new HashMap<EncodeHintType, Object>();
+        hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, size, size, hintMap);
+
+        int matrixWidth = bitMatrix.getWidth();
+        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+        graphics.setColor(Color.BLACK);
+
+        for (int i = 0; i < matrixWidth; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (bitMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+        ImageIO.write(image, fileType, qrFile);
+    }
+
+    /**
+     * 读取二维码网址
+     */
+    public static String scanQRCode(String filePath) {
+        String url = "";
+        try {
+            File file = new File(filePath);
+            BufferedImage image = ImageIO.read(file);
+            LuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(), getPixels(image));
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            Result result = new MultiFormatReader().decode(bitmap);
+            url = result.getText();
+            System.out.println("二维码中的网址为：" + url);
+        } catch (IOException | ReaderException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    //获取图像的像素数据
+    private static int[] getPixels(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[] pixels = new int[width * height];
+        image.getRGB(0, 0, width, height, pixels, 0, width);
+        return pixels;
+    }
+
+    /**
+     * 根据网址跳转
+     */
+    public static void dropUrl(String url) {
+        try {
+            //调用系统默认浏览器打开指定URL
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试生成一张指定地址的二维码
+     */
+    public static void main(String[] args) {
+
+        String url = "http://baidu.com";
+        int size = 300;
+        String fileType = "png";
+        File qrFile = new File("./template/healthQrcode.png");
+
+        try {
+            //调用生成二维码
+            createQRCode(url, size, fileType, qrFile);
+            System.out.println("QR code generated successfully!");
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
